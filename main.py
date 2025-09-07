058ca57cfd8cd2593cf24b16e7485535de3e88a8
Cezar Miranda <cezar611@gmail.com>
2025-09-07 12:01:49 -0300

    create main.py with detect_enconding function and file structure

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import chardet
+
+"""
+1.	Identificar o encoding do arquivo;
+2.	Processar o arquivo de 1000 em 1000 linhas;
+3.	Remover os espaços das colunas. Ex. ' Porto Alegre ' -> 'Porto Alegre';
+4.	Criar uma coluna CITY_ASCII no arquivo, a qual deve ser construída com base na coluna CITY. Esta coluna não pode conter acentos, minúsculas e caracteres especiais. Apenas letras, números e hífen são permitidos. Ex. 'São Paulo - abç' -> 'SAO PAULO - ABC'
+5.	Remover os caracteres não numéricos da coluna PHONE; 
+6.	Salvar o arquivo .csv com a nova coluna em UTF-8;
+
+"""
+def detect_encoding(path_file: str, sample_size: int = 100000) -> str:
+    #Identificar o encoding do arquivo;
+
+    with open(path_file, 'rb') as f:
+        result = chardet.detect(f.read(sample_size))
+    return result.get('encoding', 'utf-8')
+
+
+encode = detect_encoding('natal2025.csv')
+
+print(encode)
