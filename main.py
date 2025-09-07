d4a01baff7bcb1fddb749e2eb456373e2d08deca
Cezar Miranda <cezar611@gmail.com>
2025-09-07 13:33:33 -0300

    create function to process all functions and create a chunksize to process the data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import chardet
 import unidecode
 import re
+from pathlib import Path
 
 """
 2.	Processar o arquivo de 1000 em 1000 linhas;
@@ -19,7 +20,7 @@ def detect_encoding(path_file: str, sample_size: int = 100000) -> str:
     return result['encoding']
 
 
-caminho = '01-bronze-raw/natal2025.csv'
+path = '01-bronze-raw/natal2025.csv'
 
 def remove_spaces(df):
     "remove spaces"
@@ -54,15 +55,27 @@ def clean_numbers (df, phone_column='PHONE'):
     
     return df
 
+def process_file(path_enter, path_out, chuncksize=1000):
+
+    detected_encode = detect_encoding(path_enter)
+    print(f'Processando : {path_enter} (encoding: {detected_encode})')
+
+    processed_chunks = []
+
+    for chunk in pd.read_csv(path_enter, chunksize=chuncksize, encoding=detected_encode):
+        chunk = remove_spaces(chunk)
+        chunk = create_column(chunk)
+        chunk = clean_numbers(chunk)
+        processed_chunks.append(chunk)
+
+    df_final = pd.concat(processed_chunks, ignore_index=True)    
+
+    df_final.to_csv(path_out, index=False, encoding='utf-8')
+    print(f'Arquivo salvo em: {path_out}')
 
 
-df = pd.read_csv(caminho)
 
-print("Antes:")
-print(df.head()) 
-
-
-df = clean_numbers(df, phone_column='PHONE')
-
-print("\nDepois:")
-print(df.head())
+if __name__ == "__main__":
+    enter_path = "01-bronze-raw/natal2025.csv"
+    out_path = "02-silver-validated/natal2025_trusted.csv"
+    process_file(enter_path, out_path)
